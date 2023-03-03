6b485dcf48f949240abfd0256e446b8f2dddcd0e
PierrickHOUEE <houee.pierrick@gmail.com>
2023-03-03 18:07:53 +0100

    Cool

--- Components/Navigation/promotion.tsx
+++ Components/Navigation/promotion.tsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import classes from "./promotion.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 function Navigation(props: any) {
   const text = { DE: {}, EN: {} };
   return (
     <div className={classes.navigation}>
       <div>
+        <Link href="/client/promotion/">Promosc</Link>
         <FontAwesomeIcon icon="list"></FontAwesomeIcon>
       </div>
     </div>
